b80e6fa0a0341f7c501bb96619b8551390a55433
charlyy <cyoon90@yahoo.com>
2014-06-13 15:03:18 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Picpoint::Application.config.secret_key_base = '16eb6be1ded70952080f0ba078bb1898c5dd558ffc11d9616ef42a670ac6d5e72d4b4fa681481f19b561b210672f1ff0dcba8df53ed815f1beac21b8d37cda64'
