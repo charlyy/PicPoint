17e3b1784a736ca75a364f19f83ffbcdf43a1480
Dean Mandile <Deanskee@Deans-MacBook-Pro.local>
2014-06-13 18:44:30 -0700

    instagram doesn't work yet but route to index is working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+   def index
+     @photo = Photo.all
+     @photos = Instagram.tag_recent_media("#{hockey}", {:count => 8})
+   end
 end
